5a9d69e03a9c4efbacc68a7a9b021b4501a319ab
VIKRAM MALAGI <malagivikram@gmail.com>
2023-06-04 14:22:42 +0530

    someChanges

--- src/practiceSel/HandleWins.java
+++ src/practiceSel/HandleWins.java
@@ -9,51 +9,54 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-	
+
 public class HandleWins {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://www.orangehrm.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		String parentWin=driver.getWindowHandle();
-		//test
-		
-		System.out.println("Title of Parent Window is "+" : "+driver.getTitle());
-		
-		WebElement element1=driver.findElement(By.xpath("/html/body/footer/section/div[2]/div/div/div[1]/div/ul/li[1]/a"));
-		WebElement element2=driver.findElement(By.xpath("/html/body/footer/section/div[2]/div/div/div[1]/div/ul/li[2]/a"));
-		WebElement element3=driver.findElement(By.xpath("/html/body/footer/section/div[2]/div/div/div[1]/div/ul/li[3]/a"));
-		WebElement element4=driver.findElement(By.xpath("/html/body/footer/section/div[2]/div/div/div[1]/div/ul/li[4]/a"));
-		
-		JavascriptExecutor js = (JavascriptExecutor)driver;
-		
+
+		String parentWin = driver.getWindowHandle();
+		// test
+
+		System.out.println("Title of Parent Window is " + " : " + driver.getTitle());
+
+		WebElement element1 = driver
+				.findElement(By.xpath("/html/body/footer/section/div[2]/div/div/div[1]/div/ul/li[1]/a"));
+		WebElement element2 = driver
+				.findElement(By.xpath("/html/body/footer/section/div[2]/div/div/div[1]/div/ul/li[2]/a"));
+		WebElement element3 = driver
+				.findElement(By.xpath("/html/body/footer/section/div[2]/div/div/div[1]/div/ul/li[3]/a"));
+		WebElement element4 = driver
+				.findElement(By.xpath("/html/body/footer/section/div[2]/div/div/div[1]/div/ul/li[4]/a"));
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
 		js.executeScript("arguments[0].click();", element1);
 		js.executeScript("arguments[0].click();", element2);
 		js.executeScript("arguments[0].click();", element3);
 		js.executeScript("arguments[0].click();", element4);
-		
-		
-		Set<String> wins=driver.getWindowHandles();
-		
-		Iterator<String> it=wins.iterator();
-		
-		while(it.hasNext()) {
-			
-			String childWins=it.next();
-			
-			if(!parentWin.equals(childWins)) {
+
+		Set<String> wins = driver.getWindowHandles();
+
+		Iterator<String> it = wins.iterator();
+
+		while (it.hasNext()) {
+
+			String childWins = it.next();
+
+			if (!parentWin.equals(childWins)) {
 				driver.switchTo().window(childWins);
-				System.out.println("Title of Child Window is "+" : "+driver.getTitle());
+				System.out.println("Title of Child Window is " + " : " + driver.getTitle());
 				driver.close();
 			}
 		}
-		
+
 		driver.quit();
 
 	}
--- src/practiceSel/RobotTest.java
+++ src/practiceSel/RobotTest.java
@@ -5,26 +5,50 @@ import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class RobotTest {
 
 	public static void main(String[] args) throws IOException, Exception {
 
-		Runtime.getRuntime().exec("notepad");
-		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.orangehrm.com/");
+		driver.manage().window().maximize();
 		Robot rob=new Robot();
 		
-		Thread.sleep(1000);
+		rob.keyPress(KeyEvent.VK_CONTROL);
+		rob.keyPress(KeyEvent.VK_MINUS);
+		rob.keyRelease(KeyEvent.VK_CONTROL);
+		rob.keyRelease(KeyEvent.VK_MINUS);
 		
-		rob.keyPress(KeyEvent.VK_A);
+		rob.keyPress(KeyEvent.VK_CONTROL);
+		rob.keyPress(KeyEvent.VK_ADD);
+		rob.keyRelease(KeyEvent.VK_CONTROL);
+		rob.keyRelease(KeyEvent.VK_EQUALS);
 		
-		int j=50;
-		for(int i=1;i<=10;i++) {
-			
-			rob.mouseMove(j, 100);
-			j=j+50;
-			Thread.sleep(500);
-		}
+//		Runtime.getRuntime().exec("notepad");
+
+//		Thread.sleep(1000);
+//		
+//		rob.keyPress(KeyEvent.VK_A);
+//		
+//		int j=50;
+//		for(int i=1;i<=10;i++) {
+//			
+//			rob.mouseMove(j, 100);
+//			j=j+50;
+//			Thread.sleep(500);
+//		}
+//		
+		
+		
+		
 		
 	}
 	
